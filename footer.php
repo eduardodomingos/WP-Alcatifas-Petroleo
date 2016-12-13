2eee22aeff43fa9ec1b76ef6ae5ddab73b5750f4
Eduardo Domingos <epcdomingos@gmail.com>
2016-12-13 23:30:32 +0000

    Clean footer

--- footer.php
+++ footer.php
@@ -11,17 +11,6 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ap' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'ap' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'ap' ), 'ap', '<a href="http://www.eduardodomingos.com" rel="designer">Eduardo Domingos</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
 
 </body>
