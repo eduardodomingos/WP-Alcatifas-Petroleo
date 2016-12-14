953965b17dfb1eed865569578245163e4caa9ec0
Eduardo Domingos <epcdomingos@gmail.com>
2016-12-14 09:36:23 +0000

    Replace hardcoded stuff

--- footer.php
+++ footer.php
@@ -11,6 +11,53 @@
 
 ?>
 
+<footer class="tm-footer">
+    <div class="uk-block uk-block-secondary uk-contrast">
+        <div class="uk-container uk-container-center">
+            <div class="uk-grid uk-grid-width-medium-1-4" data-uk-grid-margin>
+                <div class="uk-panel">
+                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="uk-display-block uk-margin-bottom" rel="home">
+                        <img src="<?php echo get_template_directory_uri() . '/assets/build/img/logo-offcanvas.png' ?>" alt="<?php bloginfo( 'name' ); ?> logo">
+                    </a>
+                    <?php
+                    if( have_rows('social', 'option') ):
+                        while ( have_rows('social', 'option') ) : the_row();
+                            echo '<a href="'. get_sub_field('url') .'" class="uk-icon-button '. get_sub_field('icon_class') .' uk-margin-right"></a>';
+                        endwhile;
+                    endif;
+                    ?>
+                    <a href="#" class="uk-icon-button uk-icon-search" data-uk-offcanvas="{target:'#offcanvas-search'}"></a>
+                </div>
+                <div class="uk-panel">
+                    <h4 class="uk-panel-title">Secções</h4>
+                    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container'=> false, 'menu_id' => 'primary-menu', 'menu_class' => 'uk-list uk-list-space' ) ); ?>
+                </div>
+                <div class="uk-panel">
+                    <h4 class="uk-panel-title">Clientes</h4>
+                    <ul class="uk-list uk-list-space">
+                        <?php wp_list_categories(array('title_li'=> '')); ?>
+                    </ul>
+                </div>
+                <div class="uk-panel">
+                    <h4 class="uk-panel-title">Contacte-nos</h4>
+                    <ul class="uk-list uk-list-space">
+                        <li><?php the_field( 'street', 'option' ); ?><br>
+                            <?php the_field( 'postal_code', 'option' ); ?> <?php the_field( 'city', 'option' ); ?></li>
+                        <li>Telefone: <a href="tel:+351<?php echo str_replace(' ', '', get_field('phone', 'option')) ?>"><?php the_field( 'phone', 'option' ); ?></a><br>
+                            Fax: <?php the_field( 'fax', 'option' ); ?><br>
+                            Email: <?php the_field( 'email', 'option' ); ?>
+                        </li>
+                        <li><?php the_field( 'working_hours', 'option' ); ?></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a href="#top" class="tm-footer__to-top" data-uk-smooth-scroll=""><i class="uk-icon-arrow-up"></i></a>
+    <p class="uk-text-center uk-text-small uk-text-muted tm-footer__legal">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> - Todos os direitos reservados</p>
+    <p class="tm-sr-only">Theme developed by <a href="http://www.eduardodomingos.com/" rel="designer">Eduardo Domingos</a>.</p>
+</footer>
+
 <?php wp_footer(); ?>
 
 </body>
