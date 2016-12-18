ada605e355fdd77b4ae3bfa0503ff06bd3e18ea0
Eduardo Domingos <epcdomingos@gmail.com>
2016-12-18 19:35:01 +0000

    Replace dummy markup

--- page-quem-somos.php
+++ page-quem-somos.php
@@ -14,8 +14,46 @@
 
 get_header(); ?>
 
-	Quem Somos!
+<div class="uk-block uk-block-default uk-padding-top-remove">
+	<div class="uk-container uk-container-center">
+		<ul class="uk-breadcrumb uk-margin-top uk-margin-bottom-remove">
+			<li><a href="index.html">Início</a></li>
+			<li class="uk-active uk-text-capitalize"><span><?php the_title(); ?></span></li>
+		</ul>
+		<?php if( get_field('page_title') ): ?>
+			<h1 class="uk-margin-large-bottom"><?php echo get_field('page_title'); ?></h1>
+		<?php endif; ?>
+		<div class="uk-grid uk-grid-width-large-1-2" data-uk-grid-margin>
+			<div>
+				<?php  the_post_thumbnail( 'large', array( 'class' => 'uk-border-rounded', 'title' => get_the_title() ) ); ?>
+			</div>
+			<div>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; endif; ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="uk-block tm-block-light">
+	<div class="uk-container uk-container-center">
+		<div class="uk-grid uk-grid-large uk-grid-width-large-1-4" data-uk-grid-margin>
+			<?php
+			if( have_rows('panel') ):
+				while ( have_rows('panel') ) : the_row(); ?>
+					<div class="uk-panel uk-text-center">
+						<i class="<?php the_sub_field('icon_class');?> uk-icon-medium uk-margin-bottom"></i>
+						<h3 class="uk-panel-title"><?php the_sub_field('title'); ?></h3>
+						<p><?php the_sub_field('text'); ?></p>
+					</div>
+				<?php endwhile;
+			endif; ?>
+		</div>
+	</div>
+</div>
 
 <?php
-get_sidebar();
+
 get_footer();
