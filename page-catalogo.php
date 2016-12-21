be279bf2bd2fe7d43d9cda086a6c27c9b8ddb976
Eduardo Domingos <epcdomingos@gmail.com>
2016-12-21 21:10:15 +0000

    Add markup

--- page-catalogo.php
+++ page-catalogo.php
@@ -23,7 +23,10 @@ get_header(); ?>
 
 			<ul class="uk-subnav uk-flex uk-flex-center uk-margin-large-bottom tm-subnav-catalog" data-uk-switcher="{connect:'#my-id', animation: 'fade'}">
 				<?php
-					$categories = get_categories();
+					$categories = get_terms(
+						'category',
+						array('parent' => 0)
+					);
 					$i = 0;
 				?>
 				<?php foreach ($categories as $category): ?>
@@ -31,7 +34,34 @@ get_header(); ?>
 					<?php $i++; ?>
 				<?php endforeach; ?>
 			</ul>
-
+			<ul id="my-id" class="uk-switcher">
+				
+				<?php foreach ($categories as $category): ?>
+					<li>
+						<div class="uk-grid uk-grid-large uk-grid-width-medium-1-2 uk-grid-width-large-1-3" data-uk-grid-margin>
+						<?php
+						$args = array('parent' => $category->term_id);
+						$sub_categories = get_categories( $args );
+						?>
+						<?php foreach ($sub_categories as $sub_category): ?>
+							
+							<div>
+								<a class="uk-panel uk-panel-box uk-panel-hover uk-text-center" href="<?php echo get_category_link( $sub_category->term_id ) ?>">
+									<div class="uk-panel-teaser">
+										<?php
+											$photo = get_field('photo', 'category_'.$sub_category->term_id );
+										?>
+										<img src="<?php echo $photo['url'] ?>" alt="<?php $photo['alt'] ?>">
+									</div>
+									<h3 class="uk-panel-title"><?php echo $sub_category->name; ?></h3>
+									<p><?php echo $sub_category->description; ?></p>
+								</a>
+							</div>
+						<?php endforeach; ?>
+						</div>
+					</li>
+				<?php endforeach; ?>
+			</ul>
 		</div>
 	</div>
 
